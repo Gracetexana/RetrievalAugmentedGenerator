1ccc18cc5cb27a535c01df6e00a03efbd6288ddb
Grace Long Torales <gracetexana@gmail.com>
2024-08-11 17:42:34 -0400

    basic structure of RAG; probably lots of errors; db too big for repo

---
+++ CreateDatabase.py
@@ -0,0 +1,67 @@
+# IMPORTS
+
+## LOADING
+from langchain_community.document_loaders import DirectoryLoader, JSONLoader
+
+## SPLITTING??
+from langchain_text_splitters import CharacterTextSplitter
+
+## VECTORIZING
+import transformers
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+
+
+
+# MODIFIABLE VARIABLES
+path = "./Documents/"
+embedding_model = "mixedbread-ai/mxbai-embed-large-v1"
+
+
+
+
+# CODE
+
+## LOADING
+def metadata_func(record : dict, metadata : dict) -> dict:
+  metadata = record.get("cveMetadata")
+  
+  return metadata
+
+loader = DirectoryLoader(
+  path, 
+  glob = "**/*.json",
+  silent_errors = True,
+  loader_cls = JSONLoader,
+  loader_kwargs = {
+    "jq_schema" : ".",
+    "content_key" : ".containers.cna",
+    "is_content_key_jq_parsable" : True,
+    "metadata_func" : metadata_func,
+    "text_content" : False
+  },
+  recursive = True
+)
+
+docs = loader.load()
+
+## SPLITTING
+text_splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=0)
+documents = text_splitter.split_documents(docs)
+
+## VECTORIZING
+embeddings = HuggingFaceEmbeddings(
+  model_name = embedding_model, 
+  model_kwargs = {
+    "device" : "cuda"
+  }
+)
+
+db = Chroma.from_documents(
+  documents, 
+  embeddings,
+  persist_directory = "./ChromaDB"
+)
+
+print(db.get())
--- RAG.py
+++ RAG.py
@@ -1,19 +1,18 @@
 """
 CHANGE AS NEEDED
 """
-
 # enter Hugging Face auth code
 hfAuth = "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" #read only
 hfAuth = "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" #fine-grained
 
 # enter model that generator is based on (as seen on Hugging Face)
-generatorModel = 'meta-llama/Meta-Llama-3-8B'
+llmModel = 'meta-llama/Meta-Llama-3-8B'
 
-# enter model that retriever is based on (as seen on Hugging Face)
-retrieverModel = "sentence-transformers/all-mpnet-base-v2"
+# enter model that embeds and decodes documents in database (as seen on Hugging Face)
+embeddingModel = "mixedbread-ai/mxbai-embed-large-v1"
 
 # enter location of vector database
-dbLoc = "/shared/rc/malont/acsac/faiss_index"
+dbLoc = "./ChromaDB"
 
 # enter the location of the questions you wish to ask
 questionsLoc = "/home/gtl1500/CyberAdvisory/llama2Script/RAGQA.xlsx"
@@ -21,30 +20,42 @@ questionsLoc = "/home/gtl1500/CyberAdvisory/llama2Script/RAGQA.xlsx"
 
 
 
+
 """
 IMPORTS
 """
-
-# necessary for full script
+# FULL SCRIPT
 import torch
 
-# genertor
+# RAG
+from langchain_core.runnables import RunnableLambda # also in GENERATORS
+
+# LLM
 import transformers
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from transformers import AutoTokenizer
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 
-# retriever
+# RETRIEVER
+## create_retriever()
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+## access_chromaDB()
+from langchain_chroma import Chroma
+from langchain.retrievers.self_query.chroma import ChromaTranslator
+## db_embeddings()
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
 
-# put them together to create a RAG
-#from langchain import PromptTemplate
+# GENERATORS
+## standalone_question_generator()
+from langchain_core.runnables.passthrough import RunnablePassthrough # also in PROMPTS.task_prompt()
+## json_query_generator()
+from langchain.chains.query_constructor.base import StructuredQueryOutputParser
+
+# PROMPTS
 from langchain_core.prompts import PromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
 
-# question answering
-import pandas as pd
+# OUTPUT PROCESSING FUNCTIONS
+## no imports
+
 
 
 
@@ -52,40 +63,90 @@ import pandas as pd
 """
 SCRIPT
 """
+if __name__ == "__main__":
+    main()
 
-# RETRIEVER
-def createRetriever(retrieverModel):
-  """
-  creates the retriever portion of the RAG (retrieval augmented generator) based on selected model form HuggingFace
-  retriever uses query to retrieve relevant documents from database (FAISS, in this case)
-  """
-  # access database
-  dbEmbeddings = HuggingFaceEmbeddings(
-      model_name = retrieverModel,
-      model_kwargs = {
-          "device": "cuda"
-      }
+def main():
+  llm = create_llm(llmModel)
+  
+  dbEmbeddings = db_embeddings(embeddingModel)
+  
+  db, translator = access_chromaDB(
+    dbLoc, 
+    dbEmbeddings
   )
   
-  database = FAISS.load_local(
-      dbLoc,
-      dbEmbeddings,
-      allow_dangerous_deserialization = True
+  retriever = create_retriever(
+    llm, 
+    db, 
+    translator
   )
   
-  retriever = database.as_retriever(
-    #search_type="similarity_score_threshold", 
-    #search_kwargs={
-    #"score_threshold": 0.7,
-    #"k": 4
-    #}
+  question = "Does the vulnerability described in CVE-2024-0011 allow for the execution of arbitrary code on the affected system?"
+  
+  chat_history = rag(
+    question,
+    llm,
+    retriever
   )
   
-  return retriever
+  question = "Anything else to add?"
   
+  chat_history = rag(
+    question,
+    llm,
+    retriever,
+    chat_history
+  )
   
-# GENERATOR
-def createGenerator(generatorModel):
+
+
+
+
+"""
+FUNCTIONS
+"""
+
+# RAG
+def rag(
+  question,
+  llm,
+  retriever,
+  chat_history = [],
+  task = "qa",
+  audience = "a general audience"
+):
+  if (len(chat_history) > 0):
+    standalone_question = standalone_question_generator(llm, chat_history).invoke(question)
+    question = standalone_question
+    
+  task_prompt = task_prompt(
+    llm, 
+    retriever,
+    audience, 
+    task
+  ).invoke(question)
+  
+  rag = (
+    llm
+    | RunnableLambda(get_helpful_answer)
+  )
+  
+  llm_response = rag.invoke(task_prompt)
+  print(llm_response)
+  
+  chat_input = "Input: " + task_prompt
+  chat_output = "Output: " + llm_response
+  chat_history = [chat_input, chat_output]
+  
+  return chat_history
+
+
+
+
+
+# LLM
+def create_llm(llmModel):
   """
   creates the generator portion of the RAG (retrieval augmented generator) based on selected model from HuggingFace
   generator uses query + documents from retriever to generate appropriate response
@@ -95,109 +156,367 @@ def createGenerator(generatorModel):
     bnb_4bit_quant_type='nf4',
     bnb_4bit_use_double_quant=True,
     bnb_4bit_compute_dtype=torch.bfloat16
-)
-  '''
-  generatorPipe = AutoModelForCausalLM.from_pretrained(
-    generatorModel,
-    token = hfAuth,
-    #temperature = 0.1,
-    #max_new_tokens = 1024,
-    #repetition_penalty = 1.1,
-    #torch_dtype = torch.bfloat16,
-    quantization_config = bnb_config,
-    #low_cpu_mem_usage = True
   )
   
+  tokenizer = AutoTokenizer.from_pretrained(llmModel)
   
-  generatorPipe = transformers.pipeline(
-      token = hfAuth,
-      task = "text-generation",
-      model = generatorModel,
-      tokenizer = tokenizer,
-      torch_dtype = torch.bfloat16,
-      device_map = "auto",
-      num_return_sequences = 1,
-      eos_token_id = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")]
-  )
-  '''
-  
-  tokenizer = AutoTokenizer.from_pretrained(generatorModel)
-  
-  generatorPipe = transformers.pipeline(
+  llmPipe = transformers.pipeline(
     "text-generation", 
-    model=generatorModel, 
+    model=llmModel, 
     model_kwargs={
       "torch_dtype": torch.bfloat16
     }, 
     device_map="auto",
-    eos_token_id = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")],
+    eos_token_id = [
+      tokenizer.eos_token_id, 
+      tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ],
     token = hfAuth
-)
+  )
   
-  
-  generator = HuggingFacePipeline(
+  llm = HuggingFacePipeline(
       pipeline = generatorPipe,
       model_kwargs = {"temperature": 0.1}
   )
   
-  return generator
+  return llm
 
 
-def createPrompt(promptText):
-  
-  template = """
-  <|begin_of_text|>
-  <|start_header_id|>system<|end_header_id|>
-  {promptText}
-  <|eot_id|>
-  <|start_header_id|>user<|end_header_id|>
-  Context: 
-  {context}
-  
-  Question: {input}
-  <|eot_id|>
-  <|start_header_id|>assistant<|end_header_id|>
-  """
-  
-  template = """You are a digital assistant that simply and thoroughly explains cybersecurity concepts. Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use five sentences maximum. Keep the answer as concise as possible. 
-  
-  Context:
-  
-  {context}
 
-  Question: {input}
-
-  Digital Assistant:"""
-  
-  prompt = PromptTemplate(
-    template = template,
-    partial_variables = {"promptText": promptText},
-    input_variables = ["context", "input"]
+# RETRIEVER
+def create_retriever(llm, db, translator):
+  retriever = SelfQueryRetriever(
+    query_constructor = json_query_generator(llm),
+    vectorstore = db,
+    structured_query_translator = translator,
+    verbose = True
   )
   
-  return prompt
+  return retriever
   
-def createRAG(retrieverModel, generatorModel, promptText = "Answer any questions in layman's terms based solely on the context: "):  
-  retriever = createRetriever(retrieverModel)
-  generator = createGenerator(generatorModel)
-  prompt = createPrompt(promptText)
   
-  response = create_stuff_documents_chain(generator, prompt)
-  RAG = create_retrieval_chain(retriever, response)
-  return RAG
+
+def access_chromaDB(dbLoc, dbEmbeddings):
+  db = Chroma(
+    persist_directory = dbLoc,
+    embedding_function = dbEmbeddings
+  )
+  
+  translator = ChromaTranslator()
+  
+  return db, translator
+
+
+def db_embeddings(embeddingModel):
+  dbEmbeddings = HuggingFaceEmbeddings(
+      model_name = embeddingModel,
+      model_kwargs = {
+          "device": "cuda"
+      }
+  )
+  
+  
+  
+  
+  
+# GENERATORS
+def standalone_question_generator(llm, chat_history):
+  standalone_question_generator = (
+    {"chat_history" : chat_history, "question" : RunnablePassthrough()}
+    | standalone_question_prompt()
+    | RunnableLambda(get_standalone_question)
+  )
+  
+  return standalone_question_generator
 
 
 
-"""
-SCRIPT
-"""
-query = "Are there any devices on my network that are vulnerable to cross-site scripting (XSS) attacks, and how can I protect against these attacks?"
+def json_query_generator(llm):
+  output_parser = StructuredQueryOutputParser.from_components()
+  
+  json_query_generator = (
+    query_constructor_prompt()
+    | llm 
+    | RunnableLambda(get_final) 
+    | output_parser
+  )
+  
+  return json_query_generator
+  
+  
+  
 
-#retriever = createRetriever(retrieverModel)
-#print(retriever.invoke(query))
 
-#generator = createGenerator(generatorModel)
-#print(generator(query))
+# PROMPTS
+def task_prompt(
+  llm, 
+  retriever,
+  audience, 
+  task
+):
+  variables = {
+      "audience" : audience,
+      "context" : retriever | format_docs
+    }
+  
+  if (task == "qa"):
+    variables["question"] = RunnablePassthrough()
+    prompt = qa_prompt()
+    
+  else if (task == "cons"):
+    prompt = cons_prompt()
+  
+  else if (task == "sc"):
+    prompt = sc_prompt()
+    
+  else:
+    print("Task not recognized. Defaulting to question-answering. Task should be one of the following: \n\nqa (for question-answering), \ncons (to outline consequences), or \nsc (for scenario creation).")
+    variables["question"] = RunnablePassthrough()
+    prompt = qa_prompt()
+    
 
-RAG = createRAG(retrieverModel, generatorModel)
-print(RAG.invoke({"input": query}).get("answer"))
+  task_prompt = (
+    variables
+    | prompt
+    
+  return task_prompt
+    
+
+
+def qa_prompt():
+  prompt_template = """
+    Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Tailor your response as if speaking to {audience}.\n\n{context}\n\nQuestion: {question}\nHelpful Answer: 
+  """
+  
+  return PromptTemplate(
+    input_variables = ["audience", "context", "question"],
+    template = prompt_template
+  )
+  
+  
+  
+def cons_prompt():
+  prompt_template = """
+    Start with a one sentence synopsis of the following context. For cybersecurity within the power industry, describe the ramifications of an attack as outlined in the context being ignored. Be specific about how these ramifications come about. Tailor your response as if speaking to {audience}.\n\n{context}\n\nHelpful Answer: 
+  """
+  
+  return PromptTemplate(
+    input_variables = ["audience", "context"],
+    template = prompt_template
+  )
+
+
+
+def sc_prompt():
+  prompt_template = """
+    Use the following pieces of context to describe a potential attack that could be faced within the power industry. Be specific in who the attacker is and what they are doing so that the security team can discuss an appropriate response. Tailor your response as if speaking to {audience}.\n\n{context}\n\nHelpful Answer: 
+  """
+  
+  return PromptTemplate(
+    input_variables = ["audience", "context"],
+    template = prompt_template
+  )
+  
+  
+  
+def standalone_question_prompt():
+  prompt_template = """
+  Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+  
+  Chat History:
+  {chat_history}
+  
+  Follow Up Input: {question}
+  Standalone question:
+  """
+  
+  return PromptTemplate(
+    input_variables = ["chat_history", "question"],
+    template = prompt_template
+  )
+
+
+
+def query_constructor_example(i, question, query, filter_statement):
+  example = f"""
+    
+    << Example {i}. >>
+    User Query:
+    {question}
+    
+    Structured Request:
+    ```json
+    {
+        "query": "{query}",
+        "filter": "{filter_statement}"
+    }
+    ```
+  """
+  return example
+   
+   
+   
+def query_constructor_prompt():
+
+  query_format = """
+    ```json
+    {
+        "query": string \ text string to compare to document contents
+        "filter": string \ logical condition statement for filtering documents
+    }
+    ```
+  """
+  data_source : """
+    ```json
+    {
+        "content": "Cybersecurity vulnerabilities",
+        "attributes": {
+          "cveId": {
+              "description": "A unique alphanumeric identifier for the vulnerability. Format: CVE-YYYY-NNNN",
+              "type": "string"
+          }
+        }
+    }
+    ```
+  """
+  
+  examples = query_constructor_example(
+    i = 1, 
+    user_query = "Does CVE-2024-0008 affect multiple versions of the software?",
+    query = "versions",
+    filter_statement = "eq(\\\"cveId\\\", \\\"CVE-2024-0008\\\")"
+  ) + query_constructor_example(
+    i = 2, 
+    user_query = "In CVE-2024-0015, is the vulnerability due to improper input validation in the DreamService.java file?",
+    query = "description",
+    filter_statement = "eq(\\\"cveId\\\", \\\"CVE-2024-0015\\\")"
+  )
+  
+  prompt_template = f"""
+    Your goal is to structure the user's query to match the request schema provided below.
+    
+    << Structured Request Schema >>
+    When responding use a markdown code snippet with a JSON object formatted in the following schema:
+    
+    {query_format}
+    
+    The query string should contain only text that is expected to match the contents of documents. Any conditions in the filter should not be mentioned in the query as well.
+    
+    A logical condition statement is composed of one or more comparison and logical operation statements.
+    
+    A comparison statement takes the form: `comp(attr, val)`:
+    - `comp` ($eq | $ne | $gt | $gte | $lt | $lte): comparator
+    - `attr` (string):  name of attribute to apply the comparison to
+    - `val` (string): is the comparison value
+    
+    A logical operation statement takes the form `op(statement1, statement2, ...)`:
+    - `op` (and | or | not): logical operator
+    - `statement1`, `statement2`, ... (comparison statements or logical operation statements): one or more statements to apply the operation to
+    
+    Make sure that you only use the comparators and logical operators listed above and no others.
+    Make sure that filters only refer to attributes that exist in the data source.
+    Make sure that filters only use the attributed names with its function names if there are functions applied on them.
+    Make sure that filters only use format `YYYY-MM-DD` when handling date data typed values.
+    Make sure that filters take into account the descriptions of attributes and only make comparisons that are feasible given the type of data being stored.
+    Make sure that filters are only used as needed. If there are no filters that should be applied return "NO_FILTER" for the filter value.
+    
+    << Data Source >>
+    {data_source}
+      
+    
+    {examples}
+    
+    
+    << Final >>
+    User Query:
+    {{question}}
+    
+    Structured Request:
+  """
+  
+  return PromptTemplate(
+    input_variables = ["question"],
+    template = prompt_template
+  )
+
+
+
+
+
+# OUTPUT PROCESSING FUNCTIONS
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+
+def snip(
+  output, # llm output from which to snip
+  starting_characters, # a string that describes the sequence of characters to start the snip
+  include_start = False, # should starting characters be included?
+  ending_characters = null, # a string that describes the sequence of characters to end the snip
+  include_end = False # should the ending characters be included?
+):
+  starting_index = output.find( # find the beginning of your excerpt
+    value = starting_characters
+  )
+  
+  ending_index = -1 # unless specified, snip will go to end of output
+  
+  if (starting_index == -1): # beginning of excerpt can't be found
+    return "Expected characters are not present in output."
+  
+  after_starting_characters = starting_index + len(starting_characters) # index of end of starting characters
+  
+  if (not include_start): # if not including starting characters
+    starting_index = after_starting_characters # set starting index to end of starting_characters sequence
+  
+  if (ending_characters != null): # if ending_characters are specified
+    ending_index = output.find( # find them
+      value = ending_characters, 
+      start = after_starting_characters
+    )
+    
+  if (ending_index == -1): # if ending_characters can't be found or weren't specified
+    return output[starting_index:] # return starting index to end of output
+    
+  if (include_end): # if ending characters should be included
+    ending_index += len(ending_characters) #change ending_index to after ending_characters
+    
+  return output[starting_index:ending_index] # return snip
+
+
+
+def get_standalone_question(output):
+  return snip(
+    output = output,
+    starting_characters = "Standalone Question:",
+    ending_characters = "?",
+    include_end = True
+
+
+
+def get_helpful_answer(output):
+  return snip(
+    output = output, 
+    starting_characters = "Helpful Answer:",
+    ending_characters = "Final Answer:"
+  ) 
+
+
+
+def get_final(output):
+  return snip(
+    output = output,
+    starting_characters = "<< Final >>"
+  )
+  
+  
+  
+def get_json_query(output):
+  return snip(
+    output = output,
+    starting_characters = "{",
+    include_start = True,
+    ending_characters = "}",
+    include_end = True
+  )
