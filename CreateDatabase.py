c5942b774e32298909227002c126e324ec6f9dd3
Grace Long Torales <gracetexana@gmail.com>
2024-08-27 16:17:48 -0400

    database made with split json files

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -4,24 +4,39 @@
 from langchain_community.document_loaders import DirectoryLoader, JSONLoader
 
 ## SPLITTING??
-from langchain_text_splitters import CharacterTextSplitter
+import json
+from langchain_text_splitters import RecursiveJsonSplitter
+from langchain.docstore.document import Document
 
 ## VECTORIZING
 import transformers
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
 
 
 
 # MODIFIABLE VARIABLES
-path = "../Documents/" # location relative to my SLURM file, DB.sh
+path = "../../cvelistV5/cves/2024/" # location relative to my SLURM file, DB.sh
 embedding_model = "mixedbread-ai/mxbai-embed-large-v1"
 
 
 
 
 # CODE
+# INITIALIZE DATABASE
+embeddings = HuggingFaceEmbeddings(
+  model_name = embedding_model, 
+  model_kwargs = {
+    "device" : "cuda"
+  }
+)
+
+db = Chroma(
+  embedding_function = embeddings,
+  collection_name = "CVEs",
+  persist_directory = "../ChromaDB" # location relative to my SLURM file, DB.sh
+)
 
 ## LOADING
 def metadata_func(record : dict, metadata : dict) -> dict:
@@ -36,7 +51,7 @@ loader = DirectoryLoader(
   loader_cls = JSONLoader,
   loader_kwargs = {
     "jq_schema" : ".",
-    "content_key" : ".containers.cna",
+    "content_key" : ".containers",
     "is_content_key_jq_parsable" : True,
     "metadata_func" : metadata_func,
     "text_content" : False
@@ -47,19 +62,17 @@ loader = DirectoryLoader(
 docs = loader.load()
 
 ## SPLITTING
-text_splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=0)
-documents = text_splitter.split_documents(docs)
+splitter = RecursiveJsonSplitter(max_chunk_size=300)
 
-## VECTORIZING
-embeddings = HuggingFaceEmbeddings(
-  model_name = embedding_model, 
-  model_kwargs = {
-    "device" : "cuda"
-  }
-)
+for document in docs:
+  chunks = splitter.split_json(json.loads(document.page_content))
 
-db = Chroma.from_documents(
-  documents, 
-  embeddings,
-  persist_directory = "../ChromaDB" # location relative to my SLURM file, DB.sh
-)
+  for chunk in chunks:
+    chunked_doc = [Document(
+        page_content = str(chunk),
+        metadata = document.metadata
+      )]
+    
+    db.add_documents(
+      documents = chunked_doc
+    )
